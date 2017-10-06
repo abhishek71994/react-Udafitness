1adb57f0ff1bc09a056b0b1c5ed9192c239d5ebd
Abhishek Prasad <abhi_hk95@Abhisheks-MacBook-Air.local>
2017-10-06 16:27:57 +0530

    Interaction between components established

---
+++ components/AddEntry.js
@@ -0,0 +1,75 @@
+import React, { Components } from 'react'
+import { View } from 'react-native'
+import {getMetricMetaInfo} from '../utils/helpers'
+import UdaciSliders from './Udacislider'
+import UdaciSteppers from './Udacistepper'
+export default class AddEntry extends Components {
+	state={
+		run:0,
+		bike:0,
+		swim:0,
+		sleep:0,
+		eat:0,
+	}
+	increament = ( metric ) =>{
+		const { max,step } = getMetricMetaInfo(metric)
+
+		this.setState((state)=>{
+			const count = state[metric] + step;
+
+			return{
+				...state,
+				[metric]: count > max ? max : count
+			}
+		})
+	}
+	decreament = ( metric ) =>{
+
+		this.setState((state)=>{
+			const count = state[metric] - getMetricMetaInfo(metric).step;
+
+			return{
+				...state,
+				[metric]: count < 0 ? 0 : count
+			}
+		})
+	}
+	slider = (metric,value)=>{
+		this.setStep(()=>({
+			[metric]: value
+		}))
+	}
+	render(){
+		const metainfo = getMetricMetaInfo()
+		return(
+			<View>
+				{Object.keys(metainfo).map((key)=>{
+						const {getIcon, type , ...rest}=metainfo[key]
+						const value = this.state[key]
+
+						return(
+							<View key={key}>
+								{getIcon()}
+								{ type === 'sliders'
+									? <UdaciSliders
+										value= {value}
+										onChange= { (value) => {this.slider(key,value)} }
+										{...rest}
+									/>
+									:<UdaciSteppers
+										value= {value}
+										onIncreament={this.increament(key)}
+										onDecreament={this.decreament(key)}
+										{...rest}
+									/>
+
+								}
+							</View>
+							)
+
+					})
+				}
+			</View>
+			)
+	}
+}
